21cdfc5dde4c1cf61f6f01cb0cda4c51f340cf53
Situ William <situ_william@yahoo.com>
2017-12-21 14:03:55 -0500

    finished added lightswitch event

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+document.addEventListener('DOMContentLoaded',function(){
+	var lightSwitch = document.querySelector('.switch');
+	var status = document.querySelector('.status')
+	lightSwitch.addEventListener('click', function(e){
+		if (e.target.classList.contains('on')){
+			document.body.classList = 'dark';
+			e.target.classList.remove('on');
+			e.target.classList.add('off');
+			status.innerText = 'Hey who turned off my nightlight?'
 
-
+		}else{
+			document.body.classList = 'light';
+			e.target.classList.remove('off');
+			e.target.classList.add('on');
+			status.innerText = "it's so bright in here!"
+		}
+	})
+})
